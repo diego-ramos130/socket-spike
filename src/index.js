57431fb80939039e601daf0832d9a330c7c8bbc4
Diego Ramos <diego.onethreezero@gmail.com>
2018-11-11 14:41:52 -0800

    BIG socket spike boi

---
+++ src/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(request, response){
+  response.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function(socket){
+  console.log('a user connected.');
+  socket.broadcast.emit('hi');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
